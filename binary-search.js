857e7d248bbfa04da789168ba3c715a9164a6661
Iroshan <iroshanw1@educbe.ca>
2023-09-11 23:48:08 -0600

    first commit

--- binary-search.js
+++ binary-search.js
@@ -1,4 +1,11 @@
+const numberSet = [ 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 function binarySearch(array, searchTerm) {
+
+	let middleIndex = Math.floor(array.length/2);
+	
+	if 
+
 	/*
 	 ** Check the middle element, if it's higher than the searchTerm
 	 ** then check the element in between the start and the middle, otherwise
@@ -10,5 +17,8 @@ function binarySearch(array, searchTerm) {
 
 	return null;
 }
+
+binarySearch(numberSet, 9);
+
 
 module.exports = binarySearch;
--- linear-search.js
+++ linear-search.js
@@ -1,4 +1,13 @@
+const numberSet = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
 function linearSearch(array, searchTerm) {
+
+	for(let i = 0; i < array.length; i++ ){
+		if(array[i] == searchTerm){
+			return i;
+		}
+	};
+
 	/*
 	 ** Check each element from start to finish
 	 ** until you find an element that matches the search term
@@ -6,7 +15,8 @@ function linearSearch(array, searchTerm) {
 	 ** Return the index of the found element
 	 */
 
-	return null;
 }
+
+console.log(linearSearch(numberSet, 15));
 
 module.exports = linearSearch;
