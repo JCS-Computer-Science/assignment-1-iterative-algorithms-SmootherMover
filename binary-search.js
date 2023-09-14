8e15af60b43cb9ac08b3890c62cc12d925f9bc77
Iroshan Wijesekara <76120001@edu.cbe.ab.ca>
2023-09-14 11:56:22 -0600

    third commit

--- binary-search.js
+++ binary-search.js
@@ -3,26 +3,26 @@ const numberSet = [ 1, 2, 3, 4, 5, 6, 7, 8, 9];
 function binarySearch(array, searchTerm) {
 	
 	let arrayStart = 0;
-	let arrayEnd = array.length + 1	; 
-	let termLocation; 
+	let arrayEnd = array.length - 1	; 
 
 	while(arrayStart <= arrayEnd){ 
 		
 		let middleTerm  = Math.floor((arrayStart + arrayEnd)/2);
-		console.log(middleTerm);
+	
 		if(array[middleTerm] == searchTerm ){
-			termLocation = arrayEnd;
-			return true;
-		}else if(searchTerm > middleTerm){
-			arrayStart = middleTerm + 1;
-			console.log("y");
-		}else{
+
+			return middleTerm;
+
+		}else if(array[middleTerm] >searchTerm ){
+
 			arrayEnd = middleTerm - 1;
-			console.log("x");
+		
+		}else{
+
+			arrayStart = middleTerm + 1;
+		
 		}
 
-	termLocation = arrayEnd;
-	
 	}
 	
 	/*
@@ -33,11 +33,11 @@ function binarySearch(array, searchTerm) {
 	 ** repeat the above process for the subset of the array
 	 ** keep repeating with smaller and smaller subsets until you find the searchTerm
 	 */
+
 	
-	console.log(termLocation);
 }
 
 
-binarySearch(numberSet, 9);
+binarySearch(numberSet, 2);
 
 module.exports = binarySearch;
--- insertion-sort.js
+++ insertion-sort.js
@@ -1,4 +1,11 @@
+let numberSet = [2, 3, 7, 8, 9, 11, 99];
+
 function insertionSort(array) {
+
+	for (let  i = 0;   < array.length;  ++) {
+		const element = array[ ];
+		
+	}
 	/*
 	 ** For each element in the array, swap it with the element
 	 ** before it over and over as long as the element before it
